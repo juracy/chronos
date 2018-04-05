e57cd3f19e00ef5653f31d0d08244cdc21e58b65
Juracy Filho <juracy@gmail.com>
2018-04-05 07:52:59 -0300

    Diversas melhorias na estrutura da aplicação

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText()
+    return element(by.css('chronos-root h1')).getText()
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'chronos-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
