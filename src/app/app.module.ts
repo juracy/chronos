fe618491df0cda5f53e7314a3b591ab3a795a14c
Juracy Filho <juracy@gmail.com>
2018-04-11 06:11:37 -0300

    Downgrade do @angular/cli
    - Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 import { AppRoutingModule } from './app-routing.module'
 import { MaterialModule } from './material/material.module'
 import { TimerModule } from './timer/timer.module'
 
 import { AppComponent } from './app.component'
+
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
@@ -16,6 +19,7 @@ import { AppComponent } from './app.component'
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
 
     MaterialModule,
     TimerModule,
