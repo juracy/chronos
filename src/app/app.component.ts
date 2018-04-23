06d4c606b77e4113ddbb4dee9825ab10e3171fdd
Juracy Filho <juracy@gmail.com>
2018-04-23 11:54:45 -0300

    Substituição do CSS por SCSS (sass)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core'
 @Component({
   selector: 'chronos-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent { }
