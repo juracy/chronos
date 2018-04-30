2e41b8ddd3d2ce91b675bf859bf0992b2ffd1426
Juracy Filho <juracy@gmail.com>
2018-04-30 17:33:18 -0300

    Better tag to version instrumentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { consts } from '../environments/consts'
 
 @Component({
   selector: 'chronos-root',
@@ -7,5 +8,5 @@ import { Component, ChangeDetectionStrategy } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  version = '1.9'
+  version = consts.version
 }
